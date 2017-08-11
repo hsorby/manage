002255187fc3371658648e7f13884468f6942104
Hugh Sorby <h.sorby@auckland.ac.nz>
2017-08-11 13:09:34 +1200

    Alternate test for finalised installation.

--- Templates/create_config.in.cmake
+++ Templates/create_config.in.cmake
@@ -1,25 +1,15 @@
+if (EXISTS "${INSTALLATION_CONFIGS_PATH}")
+    set(_INSTALLATION_FINALISED TRUE)
+else ()
+    set(_INSTALLATION_FINALISED FALSE)
+endif ()
+
+file(GLOB_RECURSE _RESULTS RELATIVE "${CMAKE_CURRENT_BINARY_DIR}" create_config.stamp)
+message(STATUS "=============")
+message(STATUS "_RESULTS: ${_RESULTS}")
+
 if (NOT EXISTS "${CONFIG_PATH}")
     make_directory("${CONFIG_PATH}")
-endif ()
-
-if (EXISTS "${CMAKE_CURRENT_BINARY_DIR}/config")
-    set(_CONFIG_DIR "${CMAKE_CURRENT_BINARY_DIR}/config")
-elseif (EXISTS "${CMAKE_CURRENT_BINARY_DIR}/configs")
-    set(_CONFIG_DIR "${CMAKE_CURRENT_BINARY_DIR}/configs")
-endif ()
-
-file(GLOB _RESULT ${_CONFIG_DIR})
-list(LENGTH _RESULT _RES_LEN)
-message(STATUS "_RESULT: ${_RESULT}")
-message(STATUS "_RES_LEN: ${_RES_LEN}")
-if (_RES_LEN EQUAL 0)
-    set(_CONFIG_FINALISED FALSE)
-else ()
-    set(_CONFIG_FINALISED TRUE)
-endif ()
-
-if (EXISTS "${CMAKE_CURRENT_BINARY_DIR}/config")
-    message(STATUS "::::: EXISTS")
 endif ()
 
 execute_process(
@@ -30,18 +20,13 @@ execute_process(
     RESULT_VARIABLE RESULT
 )
 
-
 if ("${DOLLAR_SYMBOL}{RESULT}" STREQUAL "0")
-    if (EXISTS "${CMAKE_CURRENT_BINARY_DIR}/config")
-        message(STATUS "::::: EXISTS NOW")
-    endif ()
-    
     execute_process(
         COMMAND "${CMAKE_COMMAND}" -E make_directory "${CONFIG_PATH}/stamp"
         COMMAND "${CMAKE_COMMAND}" -E touch "${DOLLAR_SYMBOL}{STAMP_FILE}"
     )
-    if (NOT _CONFIG_FINALISED)
-        message(STATUS "Finalising configuration.")
+    if (NOT _INSTALLATION_FINALISED)
+        message(STATUS "Finalising installation.")
         execute_process(
             COMMAND "${CMAKE_COMMAND}" .
             WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
