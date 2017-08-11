3fc598a6772dab7428d487416cebd20f1485e544
Hugh Sorby <h.sorby@auckland.ac.nz>
2017-08-11 15:12:24 +1200

    Fix logic for setting the installation finalised.

--- Templates/create_config.in.cmake
+++ Templates/create_config.in.cmake
@@ -1,12 +1,13 @@
-if (EXISTS "${INSTALLATION_CONFIGS_PATH}")
-    set(_INSTALLATION_FINALISED TRUE)
-else ()
-    set(_INSTALLATION_FINALISED FALSE)
-endif ()
 
+# The installation type is not defined until the first configuration has been set.
+# Once the first configuration has been created the installation type cannot change.
 file(GLOB_RECURSE _RESULTS RELATIVE "${CMAKE_CURRENT_BINARY_DIR}" create_config.stamp)
-message(STATUS "=============")
-message(STATUS "_RESULTS: ${_RESULTS}")
+list(LENGTH _RESULTS _RES_LEN)
+if (_RES_LEN EQUAL 0)
+    set(_INSTALLATION_TYPE_DEFINED FALSE)
+else ()
+    set(_INSTALLATION_TYPE_DEFINED TRUE)
+endif ()
 
 if (NOT EXISTS "${CONFIG_PATH}")
     make_directory("${CONFIG_PATH}")
@@ -25,10 +26,10 @@ if ("${DOLLAR_SYMBOL}{RESULT}" STREQUAL "0")
         COMMAND "${CMAKE_COMMAND}" -E make_directory "${CONFIG_PATH}/stamp"
         COMMAND "${CMAKE_COMMAND}" -E touch "${DOLLAR_SYMBOL}{STAMP_FILE}"
     )
-    if (NOT _INSTALLATION_FINALISED)
+    if (NOT _INSTALLATION_TYPE_DEFINED)
         message(STATUS "Finalising installation.")
         execute_process(
-            COMMAND "${CMAKE_COMMAND}" .
+            COMMAND "${CMAKE_COMMAND}" -DFINALISE_INSTALLATION=TRUE .
             WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
         )
     endif ()
